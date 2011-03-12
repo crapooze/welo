aa9bc83cd5767c36caedb6f5f1944b814a34f1fb
crapooze <crapooze@gmail.com>
2011-03-12 18:26:11 -0500

    simplified the core code for ObservationMaker

--- lib/welo/core/observation.rb
+++ lib/welo/core/observation.rb
@@ -1,21 +1,36 @@
 
+require 'derailleur'
+
 module Welo
+  # DelayedObservation is a class representing a subsequent observation.
   class DelayedObservation 
-    #Struct.new(:observation, :rel, :path)
     attr_accessor :observation, :relationship, :path
   end
 
+  # An ObservationStruct is a special Struct (i.e. a class whose 
+  # instances are structures).
   ObservationStruct = Class.new(Struct) do
-    attr_accessor :_source
-    unless members.include?(:source)
-      alias :source :_source
+    # The source for an observation instance is the object from which it could
+    # be observed. Many applications may not matter, but some do.
+    attr_accessor :_source_
+    alias :source :_source_
+
+    def self.new_for_resource_in_perspective(resource, name, &blk)
+      persp = resource.perspective(name)
+      raise ArgumentError.new("no such perspective: #{name} for #{resource}") unless persp
+      st = ObservationStruct.new(*persp.fields, &blk)
+      st.resource = resource
+      st
     end
 
     class << self
-      attr_accessor :maker, :perspective
+      # The resource of an ObservationStruct is the type 
+      # of resource it's looking at.
+      attr_accessor :resource
+
       def for_hash(hash)
         vals = members.map do |k|
-          rel = maker.resource.relationship(k)
+          rel = resource.relationship(k)
           if rel
             DelayedObservation.new(self, rel, hash[k])
           else
@@ -25,21 +40,26 @@ module Welo
         self.new(*vals)
       end
 
-      def for_source(source)
+      def for_source(src)
         raise NotImplemented
-        #TODO: figure-out a good API
+        #TODO: figure-out a good API instead of source.to_h, will depend on the source class
         obj = for_hash(source.to_h)
-        obj._source = source
+        obj._source_ = src
         obj
       end
     end
   end
 
-  class ObservationSource
-  end
-
+  # An ObseravtionMaker is a metaprogramming class whose goal is to create
+  # ObservationStruct classes for (exactly) one resource's perspectives.
   class ObservationMaker
-    attr_accessor :resource, :structures
+    # The resource from which observation are being made
+    attr_accessor :resource
+
+    # A hash to cache the structures (ObservationStruct ruby classes) 
+    # for the various perspectives.
+    attr_accessor :structures
+
     def initialize(res=nil)
       @resource = res
       @structures = {}
@@ -49,16 +69,16 @@ module Welo
       @structures[name] ||= create_structure!(name, &blk)
     end
 
-    def create_structure!(name, &blk)
-      persp = resource.perspective(name)
-      raise ArgumentError.new("no such perspective: #{name} for #{resource}") unless persp
-      st = ObservationStruct.new(*persp.fields, &blk)
-      st.maker = self
-      st.perspective = name
-      st
+    def create_structure!(persp, &blk)
+      ObservationStruct.new_for_resource_in_perspective(resource, persp, &blk)
     end
   end
 
+  class ObservationSource
+    #XXX TODO
+  end
+
+  # An Observer is an object which is responsible for creating.
   class Observer
     attr_reader :registrations, :models
     def initialize(models=[])
@@ -76,13 +96,12 @@ module Welo
         super
         #creates the tree to map the DB structure to classes paths
         models.each do |model|
-          path = model.path_model(:flat_db) #XXX allow to change the db perspective name
+          path = model.path_model(:flat_db) #XXX allow to change the db identifying name
           node = build_route(path)
           node.content = model
         end
       end
     end
-
 
     #TODO 
     # - load from one path + file
@@ -93,5 +112,4 @@ module Welo
   class HTTPObserver < Observer
     #TODO: use net/http or em-http
   end
-
 end
