9b07a9bcbccdb3398fd055a0cf9bc0e809054e62
crapooze <crapooze@gmail.com>
2011-04-10 22:55:51 -0400

    simplified example

--- example/observing.rb
+++ example/observing.rb
@@ -15,27 +15,34 @@ class Peer
   perspective :other_peer, [:uuid, :files, :cost]
 end
 
-observation_klass = Welo::ObservationStruct.new_for_resource_in_perspective(Peer, :default) do
-  def hello
-    "helo from #{uuid}"
-  end
-end
-
-class DummyObserver < Welo::Observer
-end
 RubyObjectSource = Struct.new(:item) do
   def observe
     yield item
   end
 end
 
-observer = DummyObserver.new([Peer])
+observation_klass = Welo::ObservationStruct.new_for_resource_in_perspective(Peer, :default) do
+  def hello
+    "helo from #{uuid}"
+  end
+end
+
+observer = Welo::Observer.new
 
 observer.register(:observation) do |o|
+  # the observation (a struct's instance with the fields
+  # corresponding to the observed resource/perspective
   p o
+
+  # some meta-data on this observation
+  p o.class.resource
+  p o.class.perspective
+  p o.source
+
+  # the added methods to this observation
   p o.hello
 end
 
-h = {'uuid' => '321', 'files' => [], 'cost' => 10 }
+h = {'uuid' => '321', 'files' => ['/myfile/103'], 'cost' => 10 }
 
 observer.observe_source(RubyObjectSource.new(h), observation_klass)
